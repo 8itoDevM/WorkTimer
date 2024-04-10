5a71724dd1fff30c67c7089ac822268e5e27ba3c
8itoDevM <gustavomarcolino862@gmail.com>
2024-04-10 10:00:04 -0300

    Sheets connected

--- assets/main.js
+++ assets/main.js
@@ -4,9 +4,26 @@ let sheet_range = 'A1:F1000';
 
 let full_url = 'https://docs.google.com/spreadsheets/d/' + sheet_id + '/gviz/tq?sheet=' + sheet_title + '&range=' + sheet_range;
 
+let name = 'Gustavo Marcolino';
+const date = new Date();
+let horario_inicio = date.toLocaleTimeString('pt-BR');
+//document.getElementById("hrCmc").
+
+console.log(horario_inicio);
+
 fetch(full_url)
 .then(res => res.text())
 .then(rep => {
     let data = JSON.parse(rep.substr(47).slice(0,-2));
-    console.log(data)
+
+    //console.log(data.table.rows[0].c[0].f); - Pega o Horário de entrada na tabela
+    // console.log(data.table.rows[0].c[1].f); - Pega o Horárior de saída na tabela
+    // console.log(data.table.rows[0].c[2].f); - Pega a data na tabela
+    // console.log(data.table.rows[0].c[3].v); - Pega o nome na tabela
+    // console.log(data.table.rows[0].c[4].v); - Pega o status na tabela
+    // console.log(data.table.rows[0].c[5].v); - Pega as horas semanais totais na tabela
 })
+
+function getData(){
+
+}
